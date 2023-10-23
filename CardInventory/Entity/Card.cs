b0c7a42c7bab40a7d96526d9cd173dfc6b4cdfc1
JCSalvador-NSI <jcsalvador.nelsoft@gmail.com>
2023-10-23 09:25:16 +0800

    Initial project files

---
+++ CardInventory/Entity/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardInventory.Entity
+{
+    public class Card
+    {
+        public Card()
+        {
+            //
+        }
+
+        public string SetCode { get; set; }
+        public string Name { get; set; }
+        public string Rarity { get; set; }
+        public int Quantity { get; set; }
+    }
+}

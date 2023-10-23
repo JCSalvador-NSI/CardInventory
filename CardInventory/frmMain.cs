65e61cc6a17a9e9cda00daaab69985a76a2dfc39
JCSalvador-NSI <jcsalvador.nelsoft@gmail.com>
2023-10-23 15:29:34 +0800

    UI- Add  button to modify quantity value.

--- CardInventory/frmMain.cs
+++ CardInventory/frmMain.cs
@@ -15,6 +15,9 @@ namespace CardInventory
     public partial class frmMain : Form
     {
         // Local scope variables
+        private const string INDEX_QTY_MODIFY_VALUE = "colModifyValue";
+        private const string INDEX_QTY_ADD = "colAdd";
+        private const string INDEX_QTY_REMOVE = "colRemove";
         private BindingList<Card> cardList = new BindingList<Card>();
 
         public frmMain()
@@ -26,6 +29,27 @@ namespace CardInventory
         {
             txtWikiURL.Text = @"https://yugipedia.com/wiki/Set_Card_Lists:Burst_of_Destiny_(OCG-JP)";//TODO: Remove
             dgvCardList.DataSource = new BindingSource() { DataSource = cardList };
+
+            // Add 'quantity' modifier columns
+            dgvCardList.Columns.Add(INDEX_QTY_MODIFY_VALUE, "Modifier");
+            var dictQtyModifier = new List<Tuple<string, string, string>>()
+            {
+                Tuple.Create(INDEX_QTY_ADD, "Add", "+"),
+                Tuple.Create(INDEX_QTY_REMOVE, "Remove", "-"),
+            };
+            
+            foreach (var pair in dictQtyModifier)
+            {
+                DataGridViewButtonColumn btnColumn = new DataGridViewButtonColumn()
+                {
+                    Name = pair.Item1,
+                    HeaderText = pair.Item2,
+                    Text = pair.Item3,
+                    Visible = true,
+                    UseColumnTextForButtonValue = true,
+                };
+                dgvCardList.Columns.Add(btnColumn);
+            }
 
             cardList.Add(new Card("SET-001", "Sample Name", "Jap", "Common", "None"));
         }

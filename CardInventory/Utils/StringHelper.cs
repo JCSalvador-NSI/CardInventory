178247468385d3cac94363db45eed501f5c0bef1
JCSalvador-NSI <jcsalvador.nelsoft@gmail.com>
2023-10-25 10:41:17 +0800

    Decode string for Card Name

---
+++ CardInventory/Utils/StringHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CardInventory.Utils
+{
+    public static class StringHelper
+    {
+        public static string DecodedHtml(this string _input)
+        {
+            return HttpUtility.HtmlDecode(_input);
+        }
+
+    }
+}

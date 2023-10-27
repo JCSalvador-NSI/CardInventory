af6d947f459aac84e9a2fc927b796c747a393193
JCSalvador-NSI <jcsalvador.nelsoft@gmail.com>
2023-10-27 13:57:35 +0800

    Add class for Extension methods

---
+++ CardInventory/Utils/ExtList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardInventory.Utils
+{
+    public static class ExtList
+    {
+        public static bool IsNotEmpty<T>(this ICollection<T> list)
+        {
+            return list != null && list.Count > 0;
+        }
+    }
+}

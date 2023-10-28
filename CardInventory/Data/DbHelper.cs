e4fe7bfd2df8afb8ee8f217b14b0cbe83f7ae715
JCSalvador-NSI <jcsalvador.nelsoft@gmail.com>
2023-10-28 14:40:32 +0800

    WIP - Add DbHelper class

---
+++ CardInventory/Data/DbHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+
+namespace CardInventory.Data
+{
+    public static class DbHelper
+    {
+    }
+}
